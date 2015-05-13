170e84f4a9caabf05bf8d0309c7c5c74a5dc5f76
Christopher Ottesen <christopher.ottesen@yahoo.com>
2015-05-13 13:13:56 +0200

    Lagt til views

---
+++ NyheterApp/NyheterApp/Controllers/AdminController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NyheterApp.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        //LAgnyhet siden
+
+        public ActionResult LagNyhet()
+        {
+
+            return View();
+        }
+
+
+        //Vis nyhet
+        public ActionResult VisAlleNyheter()
+        {
+            return View();
+        }
+
+
+        //Vis en nyhet
+
+        public ActionResult VisEnNyhet() 
+        {
+            return View();
+        }
+        //Rediger nyehter
+        public ActionResult RedigerNyheter()
+        {
+
+            return View();
+        }
+
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
